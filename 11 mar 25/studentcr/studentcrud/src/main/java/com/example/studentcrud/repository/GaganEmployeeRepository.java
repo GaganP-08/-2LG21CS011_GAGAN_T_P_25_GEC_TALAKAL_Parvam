efaf02cbbcedbfd6b7cbf5402916287b754bfbbf
gaganp27 <sp27072003@gmail.com>
2025-03-24 10:28:25 +0530

    new changes

--- 11 mar 25/studentcr/studentcrud/src/main/java/com/example/studentcrud/repository/GaganEmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.studentcrud.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.studentcrud.model.Employee;
-
-public interface GaganEmployeeRepository extends JpaRepository<Employee, Long> {
-  
-}
